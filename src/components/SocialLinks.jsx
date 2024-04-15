398eb067bd4cc5d9c28816f646834b3da0c01e57
Nikola Brasnic <nikolabrasnic@outlook.de>
2024-04-16 00:08:44 +0200

    New

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaGithub, FaLinkedin, FaInstagram} from "react-icons/fa"
+import {FaGithub, FaLinkedin} from "react-icons/fa"
 import {HiOutlineMail} from "react-icons/hi"
 import {BsFillPersonLinesFill} from "react-icons/bs"
 
